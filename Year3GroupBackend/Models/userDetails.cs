af3f0e1a37dd3018ca0a4e32f7583148e3db6fad
aidangoldie7 <40402670@live.napier.ac.uk>
2021-04-28 19:17:45 +0100

    remove validation

--- Year3GroupBackend/Models/userDetails.cs
+++ Year3GroupBackend/Models/userDetails.cs
@@ -24,54 +24,7 @@ namespace Year3GroupBackend.Models
 
 
 
-        if (string.IsNullOrEmpty(DOB))
-        {
-            Console.WriteLine("Enter your date of birth");
-
-        }
-
-         if (string.IsNullOrEmpty(course))
-        {
-            Console.WriteLine("Enter the name of your course");
-
-        }
-
-         if (string.IsNullOrEmpty(firstName))
-        {
-            Console.WriteLine("Enter your first name");
-
-        }
-
-         if (string.IsNullOrEmpty(lastName))
-        {
-            Console.WriteLine("Enter your last name");
-
-        }
-
-         if (string.IsNullOrEmpty(gender))
-        {
-            Console.WriteLine("Enter your gender");
-
-        }
-
-         if (string.IsNullOrEmpty(gradYear))
-        {
-            Console.WriteLine("Enter your graduation vibe");
-
-        }
-
-         if (string.IsNullOrEmpty(institutionName))
-        {
-            Console.WriteLine("Enter the name of your instatution");
-
-        }
-
-         if (string.IsNullOrEmpty(studentID))
-        {
-            Console.WriteLine("Enter your student ID");
-
-        }
-
     }
+}
 
     
