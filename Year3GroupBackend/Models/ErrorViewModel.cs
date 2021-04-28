4ae6a807559c3a60f744f81299eb51474ba85786
aidangoldie7 <40402670@live.napier.ac.uk>
2021-04-28 18:22:35 +0100

    Add project files.

---
+++ Year3GroupBackend/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Year3GroupBackend.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
