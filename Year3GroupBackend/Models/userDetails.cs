7fd62071060f1acd5df81abaedee946d0caa4e25
Callan <40444689@live.napier.ac.uk>
2021-04-28 19:09:49 +0100

    startofourcode

--- Year3GroupBackend/Models/userDetails.cs
+++ Year3GroupBackend/Models/userDetails.cs
@@ -5,14 +5,23 @@ namespace Year3GroupBackend.Models
 {
     public class userDetails
     {
- public int Id { get; set; }
+ public int studentID { get; set; }
         
         public string firstName { get; set; }
 
         public string lastName { get; set; }
 
-        public string email { get; set; }
+        public string gender { get; set; }
+
+        public DateTime DOB { get; set; }
        
+        public string instatutionName { get; set; }
+
+        public string course { get; set; }
+
+        public string gradYear { get; set; }
+
+        
     }
 
 
