931ad4a1959af951cc43745621236f9f81f5e1bd
aidangoldie7 <40402670@live.napier.ac.uk>
2021-04-28 20:17:03 +0100

    no empty inputs

--- Year3GroupBackend/Models/userDetails.cs
+++ Year3GroupBackend/Models/userDetails.cs
@@ -32,23 +32,54 @@ namespace Year3GroupBackend.Models
         Regex r = new Regex("^[a-zA-Z]+$");
 
 
+        //no entries can be blank
+        if (string.IsNullOrEmpty(this.studentID.ToString()))
+        {
+            Console.WriteLine("Student ID cannot be blank");
+        }
+        
         if (string.IsNullOrEmpty(this.firstName))
         {
-            Console.WriteLine("Please enter first name");
+            Console.WriteLine("First name cannot be blank");
         }
-
 
         if(string.IsNullOrEmpty(this.lastName))
         {
-            Console.WriteLine("Please enter last name");
+            Console.WriteLine("Last name cannot be blank");
+        }
+
+        if(string.IsNullOrEmpty(this.gender))
+        {
+            Console.WriteLine("Gender cannot be blank");
+        }
+
+        if(string.IsNullOrEmpty(this.DOB.ToString()))
+        {
+            Console.WriteLine("Date of Birth cannot be blank");
+        }
+
+        if(string.IsNullOrEmpty(this.institutionName))
+        {
+            Console.WriteLine("Institution name cannot be blank");
+        }
+
+        if(string.IsNullOrEmpty(this.course))
+        {
+            Console.WriteLine("Course name cannot be blank");
+        }
+
+        if(string.IsNullOrEmpty(this.gradYear))
+        {
+            Console.WriteLine("Graduation year cannot be blank");
         }
 
 
+
+        //if first name or last name contains anything other than letters
         if (!r.IsMatch(this.firstName))
         {
             Console.WriteLine("Name must only contain letters");
         }
-
 
         if (!r.IsMatch(this.lastName))
         {
